f07b40597429b2195699b0461699fad095b4c115
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-05 03:29:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number < 1
+   return false
+ elsif number <= 3 
+  return true
+else
+  i=0
+  while i*i < number
+    if number % i == 0 || number % (i + 2) == 0
+            return false
+        i = i + 1
+      end
+     return true
+   end
+ end
+end
