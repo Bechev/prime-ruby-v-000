94783b53ad7ed0a8ca350d60b27bf176347e7362
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-05 03:39:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,14 +7,16 @@ def prime?(number)
   elsif number % 2 == 0 || number % 3 ==0
     return false
   else
+
     i=2
     while i*i <= number
       if number % i == 0
-        return false
+        i += 1
       else
         return true
       end
       i += 1
     end
+    return false
   end
 end
